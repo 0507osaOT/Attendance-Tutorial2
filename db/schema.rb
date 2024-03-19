0a5addb0c2787f94ca84d1dbbcd49b1926f8bc31
坪井  理 <tsuboiri@MacBook-Pro.local>
2024-03-20 00:33:34 +0900

    zangyousinnsei OK

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# schema.rb
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,21 +12,8 @@
 
 ActiveRecord::Schema.define(version: 20240128140211) do
 
-  create_table "attendances", force: :cascade do |t|
-    t.date "worked_on"
-    t.datetime "started_at"
-    t.datetime "finished_at"
-    t.string "note"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "overtime"
-    t.string "approval"
-    t.string "overtime_content"
-    t.string "status"
-    t.string "overtime_instructor"
-    t.index ["user_id"], name: "index_attendances_on_user_id"
-  end
+# Could not dump table "attendances" because of following StandardError
+#   Unknown type 'strin' for column 'status'
 
   create_table "bases", force: :cascade do |t|
     t.string "base_number"
@@ -46,8 +32,8 @@ ActiveRecord::Schema.define(version: 20240128140211) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2024-01-27 23:00:00"
-    t.datetime "work_time", default: "2024-01-27 22:30:00"
+    t.datetime "basic_time", default: "2024-03-18 23:00:00"
+    t.datetime "work_time", default: "2024-03-18 22:30:00"
     t.string "employee_number"
     t.string "uid"
     t.string "affiliation"
@@ -55,7 +41,6 @@ ActiveRecord::Schema.define(version: 20240128140211) do
     t.datetime "designated_work_start_time", default: "2022-12-08 00:30:00"
     t.datetime "designated_work_end_time", default: "2022-12-08 09:00:00"
     t.boolean "superior", default: false
-    t.string "applicant"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
