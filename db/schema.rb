212bb3214478b5c29e55213371e6ff31c060bb08
坪井  理 <tsuboiri@MacBook-Pro.local>
2024-10-04 01:00:03 +0900

    kinntairogu kannryou

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20240923133901) do
+ActiveRecord::Schema.define(version: 20241001224723) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20240923133901) do
     t.datetime "chg_finished_at"
     t.string "work_approval"
     t.datetime "approval_date"
+    t.datetime "before_started_at"
+    t.datetime "before_finished_at"
     t.index ["user_id"], name: "index_attendances_on_user_id"
   end
 
